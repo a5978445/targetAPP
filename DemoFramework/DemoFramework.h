cb876aeac73047cb9a99ec0e67660c2b941a6068
fitch <510491354@qq.com>
2019-10-30 10:17:11 +0800

    Initial Commit

---
+++ DemoFramework/DemoFramework.h
@@ -0,0 +1,13 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by 李腾芳 on 2019/10/30.
+//  Copyright © 2019 HSBC Holdings plc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFramework : NSObject
+
+@end
---
+++ DemoFramework/DemoFramework.m
@@ -0,0 +1,13 @@
+//
+//  DemoFramework.m
+//  DemoFramework
+//
+//  Created by 李腾芳 on 2019/10/30.
+//  Copyright © 2019 HSBC Holdings plc. All rights reserved.
+//
+
+#import "DemoFramework.h"
+
+@implementation DemoFramework
+
+@end
